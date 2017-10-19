07c070ccc10f6fbc0c9a5352eb70097f6f6f2839
Hancoson <hancoson@163.com>
2017-10-19 16:42:38 +0800

    optimized code

--- app/controllers/apis.js
+++ app/controllers/apis.js
@@ -30,6 +30,7 @@ exports.postGroup = function (req, res) {
    * 按条件编辑
    * @param _id
    */
+  /*
   blogdbs.findOne({
     _id: req.params.id
   }, function (err, data) {
@@ -43,6 +44,25 @@ exports.postGroup = function (req, res) {
 
     }
   });
+  */
+  const query = {
+    _id: req.params.id
+  };
+  const options = {
+    multi: true
+  };
+  blogdbs.update(query, {
+    $set: {
+      title: req.body.title,
+      info: req.body.info
+    }
+  }, options, function (err, data) {
+    if (err) {
+      res.json({code: 0, msg: "err"});
+    } else {
+      res.json({code: 200, msg: "success", data: data, count: data.length, refer: req.params});
+    }
+  })
 
 };
 exports.addArticles = function (req, res) {
@@ -50,7 +70,7 @@ exports.addArticles = function (req, res) {
   /**
    * 新增
    */
-  var data = new blogdbs({title: req.body.title, info: req.body.info, publishTime: new Date()});
+  const data = new blogdbs({title: req.body.title, info: req.body.info, publishTime: new Date()});
 
   data.save(function (err) { // 执行保存，并查看返回情况
     if (err) {
@@ -68,7 +88,7 @@ exports.deleArticles = function (req, res) {
    * 删除
    */
   var _id = req.body.id;
-
+  /*
   blogdbs.findOne({
     _id: _id
   }, function (err, data) {
@@ -76,6 +96,16 @@ exports.deleArticles = function (req, res) {
       res.json({code: 0, msg: "err"});
     } else {
       data.remove();
+      res.json({code: 200, msg: "dele success", data: data, count: data.length, refer: req.params});
+    }
+  })
+*/
+  blogdbs.remove({
+    _id: _id
+  }, function (err, data) {
+    if (err) {
+      res.json({code: 0, msg: "err"});
+    } else {
       res.json({code: 200, msg: "dele success", data: data, count: data.length, refer: req.params});
     }
   })
