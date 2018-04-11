ca9bf018d3ce3630852dcde80472cd359f1a926b
Hancoson <hancoson@163.com>
2018-04-11 15:02:33 +0800

    add log

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
-const logger = require('morgan');
+//const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-
+const logger = require('./services/logger')
 require('./models/db')
-
 const app = express();
 
 // view engine setup
@@ -16,9 +15,10 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+//app.use(logger('dev'));
+app.use(logger.express);
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
